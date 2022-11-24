c7647599e36990a23ceeca41881d7dda387bc3eb
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2022-11-24 10:13:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S. Segundos => 10s => 10 segundos");
+            Console.WriteLine("M. Minutos => 10m => 1 minuto");
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+            while (currentTime < time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine("Time: " + currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(1500);
+        }
+    }
+}
